3c859d46a13ba2f9b7bf83e909ba2939d8e7434a
gabi <gabi0403m@gmail.com>
2026-04-09 16:08:17 -0300

    09/04 .

--- POOJavaScript/att2.js
+++ POOJavaScript/att2.js
@@ -28,3 +28,28 @@ meuCarro.ano = 2020;
 console.log("\nInformações do meu carro:\n");
 meuCarro.exibirInfo();
 
+// Crie uma subclasse Carro, que herda de Veiculo e adiciona:
+// quantidadePortas
+// Método exibirInfo(), sobrescrevendo o da classe pai para incluir as portas.
+
+class Carro extends Veiculo{
+    //atributo adicional
+    quantidadePortas;
+
+    //método sobrescrito
+    exibirInfo(){
+        super.exibirInfo(); // chama o método da classe pai
+        console.log(`Quantidade de Portas: ${this.quantidadePortas}`);
+    }
+}
+
+// exemplo de uso da subclasse
+const meuCarro2 = new Carro();
+meuCarro2.marca = "Honda";
+meuCarro2.modelo = "Civic";
+meuCarro2.ano = 2021;
+meuCarro2.quantidadePortas = 4;
+
+console.log("\nInformações do meu segundo carro:\n");
+meuCarro2.exibirInfo();
+
