19120322e985553f7f70a3268e8878c883d4e9f1
gabi <gabi0403m@gmail.com>
2026-05-19 16:02:22 -0300

    19/05

--- TypeScript/exercicio2/src/index.ts
+++ TypeScript/exercicio2/src/index.ts
@@ -1,7 +1,3 @@
-// Crie uma interface Estudante com as propriedades: nome (string) e notas (um array de números: number[]).
-// Crie uma função chamada calcularMedia que receba um Estudante, calcule a média de suas notas e exiba no console o nome do aluno, sua média formatada e se ele foi Aprovado (média maior ou igual a 7) ou Reprovado.
-// Crie um objeto de teste para validar o funcionamento.
-
 interface Estudante{
     nome: string;
     notas: number[];
